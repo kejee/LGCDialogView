04587a03d67425fa9eb52714edebc9b644ed5d89
kk <evekejie@qq.com>
2020-09-17 18:34:36 +0800

    test

---
+++ AppClipTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppClipTest
+//
+//  Created by apple on 2020/9/17.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LGCAlertView/FirstTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  FirstTableViewController.h
+//  LGCAlertView
+//
+//  Created by apple on 2020/9/17.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
